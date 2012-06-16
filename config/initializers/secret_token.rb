7a959d1c0390af08ee51c564585a7c316943a093
Srirama Manojit <nchaitanyaram@yahoo.com>
2012-06-16 19:05:10 +0530

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Panchayat::Application.config.secret_token = '97bfd866009d90f121ce4a6ebffc38e30e69caf28f4a0696166329361b1f97bcb6246ef316f9d920cfc9f881469d98984a90bd5f8b3968b79cf0da473bfe001f'
